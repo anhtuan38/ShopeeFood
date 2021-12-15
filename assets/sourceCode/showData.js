f679768be71278bf07812f8f177545d75053471f
tuan <tuanphan204@gmail.com>
2021-12-15 23:08:13 +0700

    first commit

--- assets/sourceCode/showData.js
+++ assets/sourceCode/showData.js
@@ -1,72 +1,80 @@
 var products = [
   {
-    id: 1,
     urlImg: './assets/images/items/bun-dau.jpg',
     name: 'Bún Đậu Nhà Cuội',
     price: 30000,
   },
   {
-    id: 2,
     urlImg: './assets/images/items/mi-quang.jpg',
     name: 'Mỳ Quảng My Ny',
     price: 35000,
   },
   {
-    id: 3,
     urlImg: './assets/images/items/phuc-long.jpg',
     name: 'Phúc Long',
     price: 40000,
   },
   {
-    id: 4,
     urlImg: './assets/images/items/tra-sua.jpeg',
     name: 'Trà Sữa Bobapop',
     price: 45000,
   },
   {
-    id: 5,
     urlImg: './assets/images/items/com-xoi.jpg',
     name: 'Cơm Gà Xối Mỡ',
     price: 50000,
   },
   {
-    id: 6,
     urlImg: './assets/images/items/com-tho.jpg',
     name: 'Cơm Thố Tuấn Lộc',
     price: 55000,
   },
   {
-    id: 7,
     urlImg: './assets/images/items/item1.jpg',
     name: 'Bún bò',
     price: 60000,
   },
   {
-    id: 8,
     urlImg: './assets/images/items/item2.jpg',
     name: 'Trà Sữa',
     price: 65000,
   },
   {
-    id: 9,
     urlImg: './assets/images/items/pizza.jpg',
     name: 'Pizza',
     price: 33000,
   },
   {
-    id: 10,
     urlImg: './assets/images/items/mi-quang2.jpg',
     name: 'Mì Quảng',
     price: 45000,
   },
   {
-    id: 11,
     urlImg: './assets/images/items/tocotoco.jpg',
     name: 'Trà Sữa Tocotoco',
     price: 46000,
   },
   {
-    id: 12,
+    urlImg: './assets/images/items/nuoc-xoai-ep.jpg',
+    name: 'Nước Xoài Ép',
+    price: 44000,
+  },
+  {
+    urlImg: './assets/images/items/pizza.jpg',
+    name: 'Pizza',
+    price: 33000,
+  },
+  {
+    urlImg: './assets/images/items/mi-quang2.jpg',
+    name: 'Mì Quảng',
+    price: 45000,
+  },
+  {
+    urlImg: './assets/images/items/tocotoco.jpg',
+    name: 'Trà Sữa Tocotoco',
+    price: 46000,
+  },
+  {
     urlImg: './assets/images/items/nuoc-xoai-ep.jpg',
     name: 'Nước Xoài Ép',
     price: 44000,
@@ -74,8 +82,8 @@ var products = [
 ];
 
 
-let items = products.map((product) => {
-  return `<div class="item-restaurant" id ="${product.id}">
+let items = products.map((product, index) => {
+  return `<div class="item-restaurant" id ="${index + 1}">
             <i class="fas fa-circle stocking"></i>
             <img src="${product.urlImg}" alt="item">
             <div class="name-and-address">
